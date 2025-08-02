888373291ae9ac38885743b6b9481315502f47f9
André Skjellevik <andskj@stud.ntnu.no>
2025-08-03 00:45:15 +0200

    Completed Task1

---
+++ src/Task1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Skriv inn antall cm: ");
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            int tomme = scanner.nextInt();
+            double cm = tomme * 2.54;
+            System.out.println(tomme + " er " + cm + " tommer.");
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
