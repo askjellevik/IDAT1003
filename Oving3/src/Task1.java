69284dfc92c7f16a8786013b7a60af6755e39a79
André Skjellevik <andskj@stud.ntnu.no>
2025-08-15 23:58:55 +0200

    Completed Task1

--- Oving3/src/Task1.java
+++ Oving3/src/Task1.java
@@ -1,6 +1,53 @@
+import java.util.*;
+
 public class Task1 {
 
+public static void main(String[] args) {
 
+    Scanner scanner = new Scanner(System.in);
+
+    try {
+        System.out.println("Skriv inn et tall så sjekker jeg om det er et primtall");
+        int inpTall = scanner.nextInt();
+
+        if (inpTall < 1){
+            System.out.println("Du må skrive et positivt heltall");
+        }
+        else {
+            boolean erPrimtall = true;
+
+
+            if (erPrimtall) {
+                for (int i = 2; i < inpTall; i++) {
+                    if (inpTall % i == 0) {
+                        erPrimtall = false;
+
+                    }
+                }
+            }
+
+
+
+            if (erPrimtall) {
+                System.out.println("Ja, " + inpTall + " er et primtall");
+            }
+            else {
+                System.out.println("Nei, " + inpTall + " er ikke et primtall");
+
+            }
+        }
+
+
+
+    } catch (Exception e) {
+        System.out.println("Skriv kun inn et heltall");
+        e.printStackTrace();
+    }
+    finally {
+        scanner.close();
+    }
+
+}
 
 
 
