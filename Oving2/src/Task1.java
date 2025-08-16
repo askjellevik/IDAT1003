bc1a8af8bafdff5d2e7c662c61ca90f4581ae71b
André Skjellevik <andskj@stud.ntnu.no>
2025-08-16 14:33:18 +0200

    Fixed logic

--- Oving2/src/Task1.java
+++ Oving2/src/Task1.java
@@ -1,43 +1,53 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Task1{
-
-
-    //Et år er skuddår dersom det er delelig med 4. Unntaket er hundreårene, de må være delelig
-    //med 400.
-    //Tegn aktivitetsdiagram som viser algoritmen for å finne ut om et år er skuddår. Årstallet skal
-    //leses inn fra brukeren. Sett opp testdata. Lag og prøv ut programmet.
-
+public class Task1 {
 
     public static void main(String[] args) {
 
-        System.out.println("Skriv inn et årstall fra år 0: ");
-
         Scanner scanner = new Scanner(System.in);
 
-
         try {
-            int inpAar = scanner.nextInt();
+            System.out.println("Skriv inn et tall så sjekker jeg om det er et primtall");
+            int inpTall = scanner.nextInt();
 
-            if (inpAar > 0) {
-                if (inpAar % 4 == 0 || inpAar % 100 == 0 && inpAar % 400 == 0) {
-                    System.out.println(inpAar + " er et skuddår");
+            if (inpTall < 2){
+                System.out.println("Du må skrive et positivt heltall, og ikke 1");
+            }
+            else {
+                boolean erPrimtall = true;
+
+
+                for (int i = 2; i < inpTall; i++) {
+                    if (inpTall % i == 0) {
+                        erPrimtall = false;
+
+                    }
+                }
+
+
+
+
+                if (erPrimtall) {
+                    System.out.println("Ja, " + inpTall + " er et primtall");
                 }
                 else {
-                    System.out.println(inpAar + " er ikke et skuddår");
+                    System.out.println("Nei, " + inpTall + " er ikke et primtall");
+
                 }
             }
 
-        }
-        catch (Exception e) {
-            System.out.println("Skriv inn et årstall fra år 0");
+
+
+        } catch (Exception e) {
+            System.out.println("Skriv kun inn et heltall");
             e.printStackTrace();
         }
         finally {
             scanner.close();
         }
 
-
-
     }
+
+
+
 }
