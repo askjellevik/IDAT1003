0ec5e32a40080157f1d277652b68033dd801d034
André Skjellevik <andskj@stud.ntnu.no>
2025-08-27 23:41:22 +0200

    Made class and methods

--- Oving5/src/Task1.java
+++ Oving5/src/Task1.java
@@ -1,7 +1,47 @@
-
 public class Task1 {
 
-    public static void main(String[] args) {
+    private int teller;
+    private int nevner;
 
+
+    public Task1(int teller, int nevner) {
+        this.teller = teller;
+        this.nevner = nevner;
+    }
+
+    public Task1(int teller) {
+        this.teller = teller;
+        this.nevner = 1;
+    }
+
+    public void addereBroker() {
+
+    }
+
+    public void subtrahereBroker() {
+
+    }
+
+    public void dividereBroker() {
+
+    }
+
+    public void multiplisereBroker() {
+
+    }
+
+
+
+
+
+    public static void main(String[] args) {
+        Task1 brok1, brok2;
+
+        try {
+            brok1 = new Task1(1, 1);
+        } catch (IllegalArgumentException e) {
+            brok1 = new Task1(1);
+
+        }
     }
 }
