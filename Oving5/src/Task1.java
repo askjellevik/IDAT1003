676243be306fefd5b69dc2b024419b1bdfe2eb79
André Skjellevik <andskj@stud.ntnu.no>
2025-09-06 18:48:43 +0200

    Completed task1

--- Oving5/src/Task1.java
+++ Oving5/src/Task1.java
@@ -1,12 +1,16 @@
 public class Task1 {
 
-    private int teller;
-    private int nevner;
+    private int teller, nevner;
 
 
     public Task1(int teller, int nevner) {
-        this.teller = teller;
-        this.nevner = nevner;
+        if (nevner == 0) {
+            throw new IllegalArgumentException("Nevner kan ikke være null");
+        }
+        else {
+            this.teller = teller;
+            this.nevner = nevner;
+        }
     }
 
     public Task1(int teller) {
@@ -14,19 +18,53 @@ public class Task1 {
         this.nevner = 1;
     }
 
-    public void addereBroker() {
+    public void addereBroker(Task1 brok2) {
+        teller = brok2.teller * nevner + brok2.nevner * teller;
+        nevner = brok2.nevner * nevner;
+        forkorteBrok();
 
     }
 
-    public void subtrahereBroker() {
+    public void subtrahereBroker(Task1 brok2) {
+        teller = teller * brok2.nevner - nevner * brok2.teller;
+        nevner = nevner * brok2.nevner;
+        forkorteBrok();
 
     }
 
-    public void dividereBroker() {
-
+    public void dividereBroker(Task1 brok2) {
+        teller = brok2.nevner * this.teller;
+        nevner = brok2.teller * this.nevner;
+        forkorteBrok();
     }
 
-    public void multiplisereBroker() {
+    public void multiplisereBroker(Task1 brok2) {
+        teller = brok2.teller * teller;
+        nevner = brok2.nevner * nevner;
+        forkorteBrok();
+    }
+
+    public int getTeller() {
+        return teller;
+    }
+
+    public int getNevner() {
+        return nevner;
+    }
+
+    public int finnStorsteNevner(int teller, int nevner) {
+        if (nevner == 0) {
+            return teller;
+        }
+        else {
+            return finnStorsteNevner(nevner,teller % nevner);
+        }
+    }
+
+    public void forkorteBrok() {
+        int storsteNevner = finnStorsteNevner(teller, nevner);
+        this.teller /= storsteNevner;
+        this.nevner /= storsteNevner;
 
     }
 
@@ -37,11 +75,39 @@ public class Task1 {
     public static void main(String[] args) {
         Task1 brok1, brok2;
 
-        try {
-            brok1 = new Task1(1, 1);
-        } catch (IllegalArgumentException e) {
-            brok1 = new Task1(1);
 
-        }
+
+
+        brok1 = new Task1(8, 4);
+        brok2 = new Task1(8, 2);
+        System.out.println("Brøk 1 (" + brok1.getTeller() + "/" + brok1.getNevner() + ") addert med brøk 2 (" + brok2.getTeller() + "/" + brok2.getNevner() + ")");
+        brok1.addereBroker(brok2);
+        System.out.println("=  " + brok1.getTeller() + "/" + brok1.getNevner());
+        System.out.println("---------------");
+
+        brok1 = new Task1(8, 2);
+        System.out.println("Brøk 1 (" + brok1.getTeller() + "/" + brok1.getNevner() + ") subtrahert med brøk 2 (" + brok2.getTeller() + "/" + brok2.getNevner() + ")");
+        brok1.subtrahereBroker(brok2);
+        System.out.println("=  " + brok1.getTeller() + "/" + brok1.getNevner());
+        System.out.println("---------------");
+
+        brok1 = new Task1(4, 2);
+        System.out.println("Brøk 1 (" + brok1.getTeller() + "/" + brok1.getNevner() + ") multiplisert med brøk 2 (" + brok2.getTeller() + "/" + brok2.getNevner() + ")");
+        brok1.multiplisereBroker(brok2);
+        System.out.println("=  " + brok1.getTeller() + "/" + brok1.getNevner());
+        System.out.println("---------------");
+
+        brok1 = new Task1(8, 2);
+        System.out.println("Brøk 1 (" + brok1.getTeller() + "/" + brok1.getNevner() + ") dividert med brøk 2 (" + brok2.getTeller() + "/" + brok2.getNevner() + ")");
+        brok1.dividereBroker(brok2);
+        System.out.println("=  " + brok1.getTeller() + "/" + brok1.getNevner());
+        System.out.println("---------------");
+
+
+        brok1 = new Task1(8);
+        System.out.println("Brøk 1 (" + brok1.getTeller() + "/" + brok1.getNevner() + ") dividert med brøk 2 (" + brok2.getTeller() + "/" + brok2.getNevner() + ")");
+        brok1.dividereBroker(brok2);
+        System.out.println("=  " + brok1.getTeller() + "/" + brok1.getNevner());
+
     }
 }
