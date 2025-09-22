4f1b6eb189c05707b9ca241b160cec67597526d5
André Skjellevik <andskj@stud.ntnu.no>
2025-09-22 14:51:19 +0200

    Add matrix multiplication and transpose methods to Matrix class

--- Oving6/src/oving6/Matrix.java
+++ Oving6/src/oving6/Matrix.java
@@ -35,6 +35,33 @@ public class Matrix {
         return new Matrix(newData);
     }
 
+    public Matrix multiplyMatrix(double[][] secMatrix) {
+        double[][] newData = new double[rows][secMatrix[0].length];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < secMatrix[0].length; j++) {
+                for (int k = 0; k < cols; k++) {
+                    newData[i][j] += data[i][k] * secMatrix[k][j];
+                }
+            }
+        }
+        return new Matrix(newData);
+
+    }
+
+    public Matrix transposeMatrix(double[][] data) {
+
+        double[][] newData = new double[data[0].length][data.length];
+
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data[0].length; j++) {
+                newData[i][j] = data[j][i];
+            }
+        }
+
+        return new Matrix(newData);
+    }
+
 
 
 
