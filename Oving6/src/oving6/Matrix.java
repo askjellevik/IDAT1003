ecfaf0ca114a7a47ba1ab67b40e4a455786c8ef1
André Skjellevik <andskj@stud.ntnu.no>
2025-09-22 15:44:20 +0200

    Refactor Matrix class methods to accept Matrix objects and add toString method; update Task3 for testing

--- Oving6/src/oving6/Matrix.java
+++ Oving6/src/oving6/Matrix.java
@@ -12,6 +12,9 @@ public class Matrix {
         this.rows = data.length;
         this.cols = data[0].length;
         this.data = new double[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            System.arraycopy(data[i], 0, this.data[i], 0, cols);
+        }
     }
 
     public int getRows() {
@@ -22,26 +25,26 @@ public class Matrix {
         return cols;
     }
 
-    public Matrix addMatrix(double[][] secMatrix) {
+    public Matrix addMatrix(Matrix secMatrix) {
 
         double[][] newData = new double[rows][cols];
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
-                newData[i][j] = this.data[i][j] + secMatrix[i][j];
+                newData[i][j] = this.data[i][j] + secMatrix.data[i][j];
             }
         }
         
         return new Matrix(newData);
     }
 
-    public Matrix multiplyMatrix(double[][] secMatrix) {
-        double[][] newData = new double[rows][secMatrix[0].length];
+    public Matrix multiplyMatrix(Matrix secMatrix) {
+        double[][] newData = new double[rows][secMatrix.data[0].length];
 
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < secMatrix[0].length; j++) {
+            for (int j = 0; j < secMatrix.data[0].length; j++) {
                 for (int k = 0; k < cols; k++) {
-                    newData[i][j] += data[i][k] * secMatrix[k][j];
+                    newData[i][j] += data[i][k] * secMatrix.data[k][j];
                 }
             }
         }
@@ -49,7 +52,7 @@ public class Matrix {
 
     }
 
-    public Matrix transposeMatrix(double[][] data) {
+    public Matrix transposeMatrix() {
 
         double[][] newData = new double[data[0].length][data.length];
 
@@ -61,6 +64,25 @@ public class Matrix {
 
         return new Matrix(newData);
     }
+
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < rows; i++) {
+            sb.append("[");
+            for (int j = 0; j < cols; j++) {
+                sb.append(data[i][j]);
+                if (j < cols - 1) {
+                    sb.append(", ");
+                }
+            }
+            sb.append("]\n");
+        }
+        return sb.toString();
+    }
+
+
 
 
 
--- Oving6/src/oving6/Task3.java
+++ Oving6/src/oving6/Task3.java
@@ -4,6 +4,45 @@ public class Task3 {
 
     public static void main(String[] args) {
 
+
+        double[][] Adata = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        double[][] Bdata = {
+                {9, 8, 7},
+                {6, 5, 4},
+                {3, 2, 1}
+        };
+
+        double[][] Cdata = {
+                {-9, -8, -7},
+                {-6, -5, -4},
+                {-3, -2, -1}
+        };
+
+        Matrix A = new Matrix(Adata);
+        Matrix B = new Matrix(Bdata);
+        Matrix C = new Matrix(Cdata);
+
+        System.out.println(A);
+        System.out.println(B);
+
+        System.out.println(A.addMatrix(B));
+        System.out.println(A.addMatrix(C));
+
+        System.out.println(A.transposeMatrix());
+
+        System.out.println(A.multiplyMatrix(B));
+
+
+
+
+
+
+
     }
 
 
